ea1868842c543bb7898c5261822cd094edbfd61c
Jmserge29 <Jmserge29@gmail.com>
2022-12-18 22:29:49 -0500

    change key cors

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors({ origin: 'https://stark-lowlands-28076.herokuapp.com', credentials: true }));
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://frontend-amatista.vercel.app', credentials: true }));
 app.use(cookieParser())
 app.use('/User', RouteUser)
 app.use('/Materia', RouteMaterias)
